db4988d44eea0a4399a18dded61129800dd9b889
Jean_Chen <chenyangjing@yahoo.com>
2015-05-07 17:04:19 -0400

    clean up

---
+++ db/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '91ad7a1c8685b0ac5719d11dc4448c9cb9dc5114e5e4dadbad96dcc1de0c4cf3da60d7a0e9c497cf4a3b0e8f7ccb0cf7887480d67b887bae520bfdf443cefeb5';
---
+++ dbconsole/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '83fa979782d3a4f11bb2c9ea68d1e5420d45c8047f3e51649020ec5389122e758d43bb6ba4b84156d3807950f816afc85a73a22122f42560d7b273367faf1a11';
